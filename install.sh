2ceb8f6f4a20da03a847a0764e49164acf81c368
Nicolas Traeder <traeder@codebility.com>
2016-02-29 21:54:13 +0100

    Adds more git config commands to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# enter your full name and email as args
+name=$1
+email=$2
+
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#set git editor to vim
+git config --global core.editor vim
+
+#set global user information
+git config --global user.email "$1"
+git config --global user.name "$2"
 
